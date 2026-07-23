ef0bbcb35375c7f935df71b9f9d7d338a9a6be8d
Rafael <rafa.pereira0224@gmail.com>
2026-07-23 09:14:45 -0300

    feat: adiciona vercel analytics corretamente no projeto vite

--- src/Pages/Saiyajins.jsx
+++ src/Pages/Saiyajins.jsx
@@ -5,37 +5,43 @@ function Saiyajins({ listaSaiyajins, evoluirSaiyajin }) {
   const { saiyajinsId } = useParams();
   const saiyajin = listaSaiyajins.find((s) => s.id === Number(saiyajinsId));
 
-  if (!saiyajin) return <h1 style={{ color: "white", textAlign: "center" }}>Saiyajin não encontrado</h1>;
+  if (!saiyajin)
+    return (
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Saiyajin não encontrado
+      </h1>
+    );
 
   return (
-    <div className="container-detalhe-final" style={{ backgroundImage: `url(${saiyajin.imagemFundo})` }}>
+    <div
+      className="container-detalhe-final"
+      style={{ backgroundImage: `url(${saiyajin.imagemFundo})` }}
+    >
       <div className="container-wrapper-duplo">
-        
-   
         <div className="card-estilo-padrao">
           <h2 style={{ color: "#ffcc00" }}>Sobre {saiyajin.nome}:</h2>
           <p className="texto-descricao">{saiyajin.descricao}</p>
         </div>
 
-    
         <div className="card-estilo-padrao">
           <h1>{saiyajin.nome}</h1>
           <img src={saiyajin.caminhoImagem} alt={saiyajin.nome} />
           <p>Estágio: {saiyajin.estagio}</p>
-          
+
           <div className="container-botoes">
             {saiyajin.estagio === 1 && (
-              <button 
-                className="btn-padrao btn-evoluir" 
+              <button
+                className="btn-padrao btn-evoluir"
                 onClick={() => evoluirSaiyajin(saiyajin.id)}
               >
                 Evoluir
               </button>
             )}
-            <Link to="/Rafael-react/" className="btn-padrao btn-voltar">Voltar</Link>
+            <Link to="/" className="btn-padrao btn-voltar">
+              Voltar
+            </Link>
           </div>
         </div>
-
       </div>
     </div>
   );
