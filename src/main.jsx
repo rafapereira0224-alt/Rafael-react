a071b346ac0f8019be68bb15b06bf41a052c6670
Rafael <rafa.pereira0224@gmail.com>
2026-07-23 08:43:28 -0300

    fix: remove vercel analytics para corrigir o build do vite

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import TagManager from 'react-gtm-module'
-import { Analytics } from "@vercel/analytics/next"
 
 TagManager.initialize({
   gtmId: "GTM-PL8MLMRK"
@@ -12,6 +11,5 @@ TagManager.initialize({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
